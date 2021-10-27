55c912fd46b514a4a7a0c2d03087a31c98642e3b
Aaron Q <aaron.quijada99@gmail.com>
2021-10-27 19:32:56 +0200

    First commit

---
+++ app/src/main/java/com/aqa/proyectoclase/vista/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aqa.proyectoclase.vista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aqa.proyectoclase.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
